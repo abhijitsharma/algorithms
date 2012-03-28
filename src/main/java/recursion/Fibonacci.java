32c9cf5670ed9f3fa110e3fac97d026a34cdaca9
Abhijit Sharma <abhijit.sharma@gmail.com>
2012-03-28 20:40:17 +0100

    tests for Fibonacci

--- src/main/java/recursion/Fibonacci.java
+++ src/main/java/recursion/Fibonacci.java
@@ -32,44 +32,4 @@ public class Fibonacci {
             return i;
         }
     }
-
-
-    public static void main(String[] args) {
-        testAll();
-    }
-
-
-    private static void testAll() {
-        testAll(0, 0l);
-        testAll(1, 1l);
-        testAll(2, 1l);
-        testAll(3, 2l);
-        testAll(4, 3l);
-        testAll(5, 5l);
-        testAll(6, 8l);
-        testAll(7, 13l);
-        testAll(20, 6765l);
-    }
-
-    private static void testAll(int n, long e) {
-        testRecursive(n, e);
-        testRecursiveMemoize(n, e);
-    }
-
-    private static void testRecursive(int n, long e) {
-        Fibonacci fib = new Fibonacci();
-        long r = fib.fibRecursive(n);
-        if (r != e) {
-            throw new RuntimeException("Unexpected result " + r + " expected " + e);
-        }
-    }
-
-    private static void testRecursiveMemoize(int n, long e) {
-        Fibonacci fib = new Fibonacci();
-        long r = fib.fibRecursiveMemoize(n);
-        if (r != e) {
-            throw new RuntimeException("Unexpected result " + r + " expected " + e);
-        }
-    }
-
 }
